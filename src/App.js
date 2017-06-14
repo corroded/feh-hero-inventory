883fd0ebb22b9f6e0fafafc727b0680a8e9831b9
Andrew Hyndman <ajhyndman@hotmail.com>
2017-06-14 20:11:48 +1000

    Demonstrate usage of proving-grounds' image server

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        {/* <HeroList heroes={heroes} /> */}
+        <HeroList heroes={heroes} />
       </div>
     )
   }
